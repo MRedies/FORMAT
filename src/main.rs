8fa924ac5040580869ee46f795a3ed3ea530d760
Matthias Redies <m.redies@fz-juelich.de>
2020-02-08 13:16:56 +0100

    tower changes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::fs;
+extern crate clap;
+use clap::App;
+
 fn main() {
-    println!("Hello, world!");
+    App::new("FORmat")
+       .version("0.1")
+       .about("Improves the readability of Fortran code")
+       .author("Matthias Redies")
+       .get_matches();
+    // --snip--
+    let filename = "bla.txt";
+    println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
 }
